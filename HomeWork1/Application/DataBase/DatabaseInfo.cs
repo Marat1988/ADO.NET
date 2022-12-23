cceaa533b1806467e5a2e20eb68758f330e9c2b3
Marat <108996479+Marat1988@users.noreply.github.com>
2022-12-23 14:51:44 +0200

    UPDATE

---
+++ HomeWork1/Application/DataBase/DatabaseInfo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    public static class DatabaseInfo
+    {
+        private static SqlConnection conn = new SqlConnection();
+        private static void OpenDataBase()
+        {
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+        }
+
+        private static void RunSql(string sql)
+        {
+            SqlDataReader rdr = null;
+            try
+            {
+                OpenDataBase();
+                conn.Open();
+                if (conn != null)
+                    Console.WriteLine("Подключение успешно установлено!\n");
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+
+                        Console.Write((rdr.FieldCount > 1) ? rdr[i] + " | " : rdr[i]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                rdr?.Close();
+                conn?.Close();
+            }
+        }
+
+        #region Отображение всей информации из таблицы с овощами и фруктами
+        public static void ShowInfoProducts()
+        {
+            RunSql("SELECT * FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Отображение всех названий овощей и фруктов
+        public static void ShowInfoNameProduct()
+        {
+            RunSql("SELECT [Name product] FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Отображение всех цветов
+        public static void ShowInfoColor()
+        {
+            RunSql("SELECT DISTINCT Color FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Показать максимальную калорийность"
+        public static void ShowInfoMaxCaloric()
+        {
+            RunSql("SELECT MAX(Caloric) FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Показать минимальную калорийность
+        public static void ShowInfoMinCaloric()
+        {
+            RunSql("SELECT MIN(Caloric) FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Показать среднюю калорийность
+        public static void ShowInfoAvgCaloric()
+        {
+            RunSql("SELECT AVG(Caloric) FROM vw_ShowInfoProduct");
+        }
+        #endregion
+        #region Показать количество овощей
+        public static void ShowInfoCountVeggie()
+        {
+            RunSql("SELECT COUNT(*) FROM TypeProducts tp INNER JOIN Product p ON tp.TypeProductId = p.TypeProductId WHERE tp.TypeProductId = 1");
+        }
+        #endregion
+        #region Показать количество фруктов
+        public static void ShowInfoCountFruit()
+        {
+            RunSql("SELECT COUNT(*) FROM TypeProducts tp INNER JOIN Product p ON tp.TypeProductId = p.TypeProductId WHERE tp.TypeProductId = 2");
+        }
+        #endregion
+
+
+    }
+}
--- HomeWork1/Application/WareHous.cs
+++ HomeWork1/Application/WareHous.cs
@@ -11,44 +11,21 @@ namespace HomeWork1
 {
     class WareHous
     {
-        private static SqlConnection conn = new SqlConnection();
-
         static void Main(string[] args)
         {
-            conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
             Console.Title = "Домашняя работа №1";
             ConsoleMenu menu = new ConsoleMenu("Главное меню");
-            menu.AddMenu("Отображение всей информации из таблицы с овощами и фруктами", ShowInfoProducts);
-            menu.AddMenu("Отображение всех названий овощей и фруктов", ShowInfoNameProduct);
-            menu.AddMenu("Отображение всех цветов", ShowInfoColor);
-            menu.AddMenu("Показать максимальную калорийность", ShowInfoMaxCaloric);
-            menu.AddMenu("Показать минимальную калорийность", ShowInfoMinCaloric);
-            menu.AddMenu("Показать среднюю калорийность", ShowInfoAvgCaloric);
+            menu.AddMenu("Отображение всей информации из таблицы с овощами и фруктами", DatabaseInfo.ShowInfoProducts);
+            menu.AddMenu("Отображение всех названий овощей и фруктов", DatabaseInfo.ShowInfoNameProduct);
+            menu.AddMenu("Отображение всех цветов", DatabaseInfo.ShowInfoColor);
+            menu.AddMenu("Показать максимальную калорийность", DatabaseInfo.ShowInfoMaxCaloric);
+            menu.AddMenu("Показать минимальную калорийность", DatabaseInfo.ShowInfoMinCaloric);
+            menu.AddMenu("Показать среднюю калорийность", DatabaseInfo.ShowInfoAvgCaloric);
+
+            menu.AddMenu("Показать количество овощей", DatabaseInfo.ShowInfoCountVeggie);
+            menu.AddMenu("Показать количество фруктов", DatabaseInfo.ShowInfoCountFruit);
+
             menu.ShowMenu();
-        }
-        private static void ShowInfoProducts()
-        {
-            Console.WriteLine("Отображение всей информации из таблицы с овощами и фруктами");
-        }
-        private static void ShowInfoNameProduct()
-        {
-            Console.WriteLine("Отображение всех названий овощей и фруктов");
-        }
-        private static void ShowInfoColor()
-        {
-            Console.WriteLine("Отображение всех цветов");
-        }
-        private static void ShowInfoMaxCaloric()
-        {
-            Console.WriteLine("Показать максимальную калорийность");
-        }
-        private static void ShowInfoMinCaloric()
-        {
-            Console.WriteLine("Показать минимальную калорийность");
-        }
-        private static void ShowInfoAvgCaloric()
-        {
-            Console.WriteLine("Показать среднюю калорийность");
         }
     }
 }
