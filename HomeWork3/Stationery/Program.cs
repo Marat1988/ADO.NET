283774327cde15f5ac0b91a6200a3b1d5a2794b7
Marat <108996479+Marat1988@users.noreply.github.com>
2022-12-28 11:10:27 +0200

    HomeWork3

---
+++ HomeWork3/Stationery/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
