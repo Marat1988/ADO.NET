e85e912beba924d6683cee00230a9b60928038bb
Marat <pirat03071988@mail.ru>
2023-01-16 07:26:42 +0200

    Еще не готово

---
+++ HomeWork4-5/Application/FormMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace HomeWork4_5
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxChooseProvider = new System.Windows.Forms.ComboBox();
+            this.labelChooseProvider = new System.Windows.Forms.Label();
+            this.buttonShowInfo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxChooseProvider
+            // 
+            this.comboBoxChooseProvider.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxChooseProvider.FormattingEnabled = true;
+            this.comboBoxChooseProvider.Location = new System.Drawing.Point(12, 44);
+            this.comboBoxChooseProvider.Name = "comboBoxChooseProvider";
+            this.comboBoxChooseProvider.Size = new System.Drawing.Size(246, 21);
+            this.comboBoxChooseProvider.TabIndex = 0;
+            this.comboBoxChooseProvider.SelectedIndexChanged += new System.EventHandler(this.comboBoxChooseProvider_SelectedIndexChanged);
+            // 
+            // labelChooseProvider
+            // 
+            this.labelChooseProvider.AutoSize = true;
+            this.labelChooseProvider.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelChooseProvider.Location = new System.Drawing.Point(12, 18);
+            this.labelChooseProvider.Name = "labelChooseProvider";
+            this.labelChooseProvider.Size = new System.Drawing.Size(246, 13);
+            this.labelChooseProvider.TabIndex = 1;
+            this.labelChooseProvider.Text = "Выберите провайдера для подключения";
+            // 
+            // buttonShowInfo
+            // 
+            this.buttonShowInfo.Location = new System.Drawing.Point(12, 80);
+            this.buttonShowInfo.Name = "buttonShowInfo";
+            this.buttonShowInfo.Size = new System.Drawing.Size(145, 36);
+            this.buttonShowInfo.TabIndex = 3;
+            this.buttonShowInfo.Text = "Информация о продуктах";
+            this.buttonShowInfo.UseVisualStyleBackColor = true;
+            this.buttonShowInfo.Click += new System.EventHandler(this.buttonShowInfo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(588, 374);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 551);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonShowInfo);
+            this.Controls.Add(this.labelChooseProvider);
+            this.Controls.Add(this.comboBoxChooseProvider);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Фабрика провайдеров, асинхронный режим доступа, конфигурационные файлы. ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxChooseProvider;
+        private System.Windows.Forms.Label labelChooseProvider;
+        private System.Windows.Forms.Button buttonShowInfo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ HomeWork4-5/Application/FormMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork4_5
+{
+    public partial class FormMain : Form
+    {
+        DbConnection conn = null;
+        DbProviderFactory fact = null;
+        string providerName = "";
+        public FormMain()
+        {
+            InitializeComponent();
+            DataTable t = DbProviderFactories.GetFactoryClasses();
+            foreach (DataRow dr in t.Rows)
+            {
+                comboBoxChooseProvider.Items.Add(dr["InvariantName"]);
+            }
+            comboBoxChooseProvider.SelectedIndex = 0;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxChooseProvider_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fact = DbProviderFactories.GetFactory(comboBoxChooseProvider.SelectedItem.ToString());
+            conn = fact.CreateConnection();
+            providerName = GetConnectionStringByProvider(comboBoxChooseProvider.SelectedItem.ToString());
+        }
+
+        private string GetConnectionStringByProvider(string providerName)
+        {
+            string returnValue = null;
+
+            //Читаем все строки подключения из App.config
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            //Ищем и возвращаем строку подключения для providerName
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    if (cs.ProviderName == providerName)
+                    {
+                        returnValue = cs.ConnectionString;
+                        break;
+                    }
+                }
+            }
+            return returnValue;
+        }
+
+        private void buttonShowInfo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.ConnectionString = providerName;
+                //Создаем адаптер из фабрики
+                DbDataAdapter adapter = fact.CreateDataAdapter();
+                adapter.SelectCommand = conn.CreateCommand();
+                adapter.SelectCommand.CommandText = "WAITFOR DELAY '00:00:10'; SELECT * FROM Product";
+                //Выполняем запрос select из адаптера
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                //Выводим результаты запроса
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                string value1 = row.Cells[0].Value.ToString();
+                string value2 = row.Cells[1].Value.ToString();
+                //...
+            }
+        }
+    }
+}
