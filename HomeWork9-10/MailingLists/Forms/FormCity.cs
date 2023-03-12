e23c3dc4a11a9395c303a4a6ce9369c8ffd2e077
Marat <pirat03071988@mail.ru>
2023-03-12 23:18:27 +0200

    UPDATE

--- HomeWork9-10/MailingLists/Forms/FormCity.cs
+++ HomeWork9-10/MailingLists/Forms/FormCity.cs
@@ -2,14 +2,10 @@
 using MailingLists.Class;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MailingLists.Forms
